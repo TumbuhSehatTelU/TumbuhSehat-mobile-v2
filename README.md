7a0f8d48e639c4b2c44402afd5e15b636562c4e6
SyahrezaAdnanAlAzhar <syahreza.adnan29@gmail.com>
2025-08-28 13:35:15 +0700

    init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# mobile_tumbuh_sehat_v2
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/mobile_tumbuh_sehat_v2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mobile_tumbuh_sehat_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
