ea24046ca16221b651906d4c7a7291583f4dfc7c
SyahrezaAdnanAlAzhar <syahreza.adnan29@gmail.com>
2025-09-08 15:37:53 +0700

    doc: add comment for BE food history json format

--- lib/domain/repositories/food_repository.dart
+++ lib/domain/repositories/food_repository.dart
@@ -9,3 +9,56 @@ abstract class FoodRepository {
   Future<Either<Failure, List<ServingSizeModel>>> getServingSizes(int foodId);
   Future<Either<Failure, void>> saveMealHistory(MealHistoryModel meal);
 }
+
+// KONTRAK BE FOOD HISTORY
+// {
+//     "familyUniqueCode": "a1b2c3d4-e5f6-7890-1234-567890abcdef",
+//     "mealTimestamp": "2023-11-21T08:45:15.123Z",
+//     "eaters": {
+//         "parents": [
+//             {
+//                 "name": "Tono",
+//                 "role": "Ayah"
+//             }
+//         ],
+//         "children": [
+//             {
+//                 "name": "Toni",
+//                 "dateOfBirth": "2022-01-10"
+//             }
+//         ]
+//     },
+//     "mealComponents": [
+//         {
+//             "foodName": "Nasi Putih",
+//             "quantity": 1.5,
+//             "urtName": "Centong",
+//             "massInGrams": 150.0,
+//             "nutritions": {
+//                 "calories": 193.5,
+//                 "protein": 3.6,
+//                 "fat": 0.3,
+//                 "carbohydrates": 41.85
+//             }
+//         },
+//         {
+//             "foodName": "Tempe Goreng",
+//             "quantity": 2.0,
+//             "urtName": "Potong Sedang",
+//             "massInGrams": 50.0,
+//             "nutritions": {
+//                 "calories": 130.0,
+//                 "protein": 11.0,
+//                 "fat": 8.0,
+//                 "carbohydrates": 5.0
+//             }
+//         }
+//     ],
+//     "totalNutritions": {
+//         "calories": 323.5,
+//         "protein": 14.6,
+//         "fat": 8.3,
+//         "carbohydrates": 46.85
+//     },
+//     "analysisMethod": "MANUAL_INPUT"
+// }
