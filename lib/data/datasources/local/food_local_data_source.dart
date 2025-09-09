89d2789c5a4de44aa81ba5b6264e2479603a29c7
SyahrezaAdnanAlAzhar <syahreza.adnan29@gmail.com>
2025-09-09 14:00:38 +0700

    change local and remote repo method on food repo

--- lib/data/datasources/local/food_local_data_source.dart
+++ lib/data/datasources/local/food_local_data_source.dart
@@ -1,53 +1,94 @@
-import 'dart:convert';
 import '../../../core/database/database_helper.dart';
-import '../../models/food_component_model.dart';
+import '../../models/child_model.dart';
+import '../../models/food_model.dart';
 import '../../models/meal_history_model.dart';
-import '../../models/serving_size_model.dart';
+import '../../models/parent_model.dart';
+import '../../models/urt_model.dart';
 
 abstract class FoodLocalDataSource {
-  Future<List<FoodComponentModel>> searchFoods(String query);
-  Future<List<ServingSizeModel>> getServingSizes(int foodId);
-  Future<void> saveMealHistory(MealHistoryModel meal);
+  Future<List<FoodModel>> searchFoods(String query);
+  Future<List<UrtModel>> getUrtsForFood(int foodId);
+  Future<void> saveMealHistory(
+    MealHistoryModel history,
+    List<ParentModel> parents,
+    List<ChildModel> children,
+  );
 }
 
 class FoodLocalDataSourceImpl implements FoodLocalDataSource {
   final DatabaseHelper dbHelper;
+
   FoodLocalDataSourceImpl({required this.dbHelper});
 
   @override
-  Future<List<FoodComponentModel>> searchFoods(String query) async {
+  Future<List<FoodModel>> searchFoods(String query) async {
     final db = await dbHelper.database;
-    final maps = await db.query(
-      'food_components',
+    final List<Map<String, dynamic>> maps = await db.query(
+      'foods',
       where: 'name LIKE ?',
       whereArgs: ['%$query%'],
       limit: 20,
     );
-    return List.generate(
-      maps.length,
-      (i) => FoodComponentModel.fromMap(maps[i]),
-    );
+
+    return List.generate(maps.length, (i) {
+      return FoodModel.fromMap(maps[i]);
+    });
   }
 
   @override
-  Future<List<ServingSizeModel>> getServingSizes(int foodId) async {
+  Future<List<UrtModel>> getUrtsForFood(int foodId) async {
     final db = await dbHelper.database;
-    final maps = await db.query(
-      'serving_sizes',
-      where: 'food_component_id = ?',
-      whereArgs: [foodId],
+    final List<Map<String, dynamic>> maps = await db.rawQuery(
+      '''
+      SELECT T2.*
+      FROM food_serving_options AS T1
+      INNER JOIN urt_conversions AS T2 ON T1.urt_id = T2.id
+      WHERE T1.food_id = ?
+    ''',
+      [foodId],
     );
-    return List.generate(maps.length, (i) => ServingSizeModel.fromMap(maps[i]));
+
+    return List.generate(maps.length, (i) {
+      return UrtModel.fromMap(maps[i]);
+    });
   }
 
   @override
-  Future<void> saveMealHistory(MealHistoryModel meal) async {
-    // NOTE: Requires new table `meal_history` in DatabaseHelper
+  Future<void> saveMealHistory(
+    MealHistoryModel history,
+    List<ParentModel> parents,
+    List<ChildModel> children,
+  ) async {
     final db = await dbHelper.database;
-    await db.insert('meal_history', {
-      'payload': json.encode(meal.toJson()),
-      'timestamp': DateTime.parse(meal.mealTimestamp).millisecondsSinceEpoch,
-      'is_synced': 0, 
+    await db.transaction((txn) async {
+      final int mealHistoryId = await txn.insert('meal_histories', {
+        'timestamp': history.timestamp,
+        'is_synced': 0,
+      });
+
+      for (final component in history.components) {
+        await txn.insert('meal_components', {
+          'meal_history_id': mealHistoryId,
+          'food_name': component.foodName,
+          'quantity': component.quantity,
+          'urt_name': component.urtName,
+          'total_grams': component.totalGrams,
+        });
+      }
+
+      for (final parent in parents) {
+        await txn.insert('meal_eaters', {
+          'meal_history_id': mealHistoryId,
+          'parent_name': parent.name,
+        });
+      }
+
+      for (final child in children) {
+        await txn.insert('meal_eaters', {
+          'meal_history_id': mealHistoryId,
+          'child_name': child.name,
+        });
+      }
     });
   }
 }
--- lib/data/datasources/remote/food_remote_data_source.dart
+++ lib/data/datasources/remote/food_remote_data_source.dart
@@ -3,27 +3,38 @@ import '../../../core/error/exceptions.dart';
 import '../../models/meal_history_model.dart';
 
 abstract class FoodRemoteDataSource {
-  Future<void> postMealHistory(MealHistoryModel meal);
+  Future<void> postMealHistory(MealHistoryModel history);
 }
 
 class FoodRemoteDataSourceImpl implements FoodRemoteDataSource {
   final Dio client;
+
   FoodRemoteDataSourceImpl({required this.client});
 
   @override
-  Future<void> postMealHistory(MealHistoryModel meal) async {
+  Future<void> postMealHistory(MealHistoryModel history) async {
     try {
-      // TODO: endpoint BE
-      final response = await client.post('/meal-history', data: meal.toJson());
-      if (response.statusCode != 200 && response.statusCode != 201) {
+      // TODO: endpoint
+      final response = await client.post(
+        '/api/meal-histories',
+        data: history.toJson(),
+      );
+
+      if (response.statusCode != null &&
+          response.statusCode! >= 200 &&
+          response.statusCode! < 300) {
+        return;
+      } else {
         throw ServerException(
           'Failed to post meal history. Status code: ${response.statusCode}',
         );
       }
     } on DioException catch (e) {
       throw ServerException(
-        e.response?.data['message'] ?? 'Unknown network error',
+        e.response?.data['message'] ?? e.message ?? 'Unknown network error',
       );
+    } catch (e) {
+      throw ServerException('An unexpected error occurred: $e');
     }
   }
 }
